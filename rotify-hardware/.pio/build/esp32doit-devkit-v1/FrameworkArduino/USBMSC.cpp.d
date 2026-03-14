b3102ac52b9473d3b2f395f0fb50222a59244507
flyingcookere <marimcln593@gmail.com>
2026-03-14 11:27:49 +0800

    keep me login- forgot password

---
+++ rotify-hardware/.pio/build/esp32doit-devkit-v1/FrameworkArduino/USBMSC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32doit-devkit-v1/FrameworkArduino/USBMSC.cpp.o: \
+ C:/Users/marim/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
+ C:/Users/marim/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ C:/Users/marim/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
